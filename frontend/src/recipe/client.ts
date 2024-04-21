4bb5536e66488adbb9c264310a65de427f844665
garnier.n@northeastern.edu <Noah Garnier>
2024-04-21 00:39:53 -0400

    Authenticate recipe requests

--- frontend/src/recipe/client.ts
+++ frontend/src/recipe/client.ts
@@ -1,12 +1,16 @@
 import axios from "axios";
 const SERVICE_URL = process.env.REACT_APP_SERVICE_URL;
 
+const api = axios.create({
+    withCredentials: true
+});
+
 export const create = async (recipe: Recipe): Promise<Recipe> => {
-    const response = await axios.post(`${SERVICE_URL}/recipe`, recipe);
+    const response = await api.post(`${SERVICE_URL}/recipe`, recipe);
     return response.data;
 }
 
 export const get = async (rid: RecipeID): Promise<Recipe> => {
-    const response = await axios.get(`${SERVICE_URL}/recipe/${rid}`);
+    const response = await api.get(`${SERVICE_URL}/recipe/${rid}`);
     return response.data;
 }
