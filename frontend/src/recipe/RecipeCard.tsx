6dd69ebc159254231300d4079682a2ed70bad0a9
garnier.n@northeastern.edu <Noah Garnier>
2024-04-22 16:42:06 -0400

    Add chef navigation and like visiblity to recipe cards

--- frontend/src/recipe/RecipeCard.tsx
+++ frontend/src/recipe/RecipeCard.tsx
@@ -1,17 +1,22 @@
-import { Card, CardBody, Stack, Heading, CardFooter, Text, Icon, useColorModeValue, Flex, Spacer } from "@chakra-ui/react";
+import { Card, CardBody, Stack, Heading, CardFooter, Text, Icon, useColorModeValue, Flex, Spacer, Button } from "@chakra-ui/react";
 import React from "react";
-import { FaHeart } from "react-icons/fa6";
+import { FaHeart, FaRegHeart } from "react-icons/fa6";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { UserState } from "../store";
 
 
 function RecipeCard({ recipe }: {recipe: Recipe }) {
   const navigate = useNavigate();
+  const { currentUser } = useSelector((state: UserState) => state.users);
   const bgColor = useColorModeValue("gray.50", "gray.700");
   const hoverColor = useColorModeValue("blue.50", "gray.600");
   const textHoverColor = useColorModeValue("blue.500", "blue.300");
 
   const nameDisplay = recipe.name.length > 17 ? recipe.name.slice(0, 17) + "..." : recipe.name;
   const descriptionDisplay = recipe.description.length > 160 ? recipe.description.slice(0, 160) + "..." : recipe.description;
+
+  const isLiked = (currentUser?.likedRecipes?.findIndex((r) => r._id === recipe._id) ?? -1) >= 0;
 
   const card = (
     <Card
@@ -35,12 +40,19 @@ function RecipeCard({ recipe }: {recipe: Recipe }) {
             <Flex align='center' justify="space-between" width="100%">
                 <Text color='red.600'>
                     {recipe.likes}
-                    <Icon ml={1} pt={1} as={FaHeart} />
+                    <Icon ml={1} pt={1} as={ isLiked ? FaHeart : FaRegHeart } />
                 </Text>
                 <Spacer />
-                <Text>
+                <Button variant='ghost' size='sm' m={0} py={0} px={1} onClick={(e) => {
+                    e.stopPropagation();
+                    if (recipe.author) {
+                        navigate(`/user/${recipe.author._id}/profile`);
+                    } else {
+                        navigate(`/recipe/${recipe._id}`);
+                    }
+                }}>
                     By {recipe.author?.username || 'Unknown'}
-                </Text>
+                </Button>
             </Flex>
         </CardFooter>
     </Card>
