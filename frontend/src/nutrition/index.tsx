62a64bf96d0b3de8e38fbfca80598df9395bfb2b
garnier.n@northeastern.edu <Noah Garnier>
2024-04-25 15:24:51 -0400

    Frontend for displaying recipes items are found in

--- frontend/src/nutrition/index.tsx
+++ frontend/src/nutrition/index.tsx
@@ -1,14 +1,17 @@
 import React, { useEffect } from 'react'
-import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Button, Center, Divider, Heading, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Button, Center, Divider, Heading, Table, Tbody, Td, Text, Th, Thead, Tr, useToast } from "@chakra-ui/react";
 import { useNavigate, useParams } from 'react-router-dom';
 import * as client from "./client";
 import { ExternalLinkIcon } from '@chakra-ui/icons';
+import RecipeCard from '../recipe/RecipeCard';
 
 function Nutrition() {
     const navigate = useNavigate();
     const { fdcId } = useParams();
     const [food, setFood] = React.useState<FDCFoodItem | null>(null);
+    const [recipes, setRecipes] = React.useState<Recipe[] | null>(null);
     const [isLoading, setIsLoading] = React.useState<boolean>(true);
+    const toast = useToast();
 
     useEffect(() => {
       console.log(`Nutrition component mounted with fdcId: ${fdcId}`);
@@ -20,18 +23,29 @@ function Nutrition() {
 
       const fetchFood = async () => {
         try {
-          const food = await client.getFoodItem(fdcId);
+          const [food, recipes] = await Promise.all([
+            client.getFoodItem(fdcId),
+            client.getRecipesWithFDCId(fdcId),
+          ]);
           console.log(food);
           setFood(food);
+          setRecipes(recipes);
         } catch (error) {
-          console.error("Error fetching food item", error);
-          navigate('/nutrition/search');
+          console.error("Error fetching food item / food item recipes", error);
+          toast({
+            title: "Error displaying food item",
+            description: "Returning to previous page",
+            status: "error",
+            duration: 5000,
+            isClosable: true,
+          });
+          navigate(-1);
         }
       };
 
       fetchFood();
       setIsLoading(false);
-    }, [fdcId, navigate]);
+    }, [fdcId, toast, navigate]);
 
     if (isLoading || !food) {
       return <></>;
@@ -64,48 +78,79 @@ function Nutrition() {
             USDA
         </Button>
       </Heading>
-      <Accordion width='100%' mt={5} allowToggle defaultIndex={0}>
-        <AccordionItem>
-          <AccordionButton>
-            <AccordionIcon ml={5} boxSize={8} />
-            <Heading
-              size='md'
-              ml={4}
-              my={5}
-            >
-              Nutritional Information
-            </Heading>
-          </AccordionButton>
-          {
-                (food.nutrients && food.nutrients.length > 0) ?
-                <AccordionPanel>
-                    <Table>
-                        <Thead>
-                            <Tr>
-                                <Th>Nutrient</Th>
-                                <Th>Amount</Th>
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            {food.nutrients.map(nutrient => (
-                                <Tr key={nutrient.nutrientId}>
-                                    <Td>{nutrient.name}</Td>
-                                    <Td>{nutrient.amount} {nutrient.unitName}</Td>
-                                </Tr>
-                            ))}
-                        </Tbody>
-                    </Table>
-                </AccordionPanel> :
-                <AccordionPanel>
-                    <Text ml={5} mb={3}>No nutritional information available for this food item</Text>
-                </AccordionPanel>
-            }
-        </AccordionItem>
-      </Accordion>
     </>
     )
 
-    return <>{headerInfo}</>;
+    const nutritionInfo = (
+      <AccordionItem>
+        <AccordionButton>
+          <AccordionIcon ml={5} boxSize={8} />
+          <Heading
+            size='md'
+            ml={4}
+            my={5}
+          >
+            Nutritional Information
+          </Heading>
+        </AccordionButton>
+        {
+              (food.nutrients && food.nutrients.length > 0) ?
+              <AccordionPanel>
+                  <Table>
+                      <Thead>
+                          <Tr>
+                              <Th>Nutrient</Th>
+                              <Th>Amount</Th>
+                          </Tr>
+                      </Thead>
+                      <Tbody>
+                          {food.nutrients.map(nutrient => (
+                              <Tr key={nutrient.nutrientId}>
+                                  <Td>{nutrient.name}</Td>
+                                  <Td>{nutrient.amount} {nutrient.unitName}</Td>
+                              </Tr>
+                          ))}
+                      </Tbody>
+                  </Table>
+              </AccordionPanel> :
+              <AccordionPanel>
+                  <Text ml={5} mb={3}>No nutritional information available for this food item</Text>
+              </AccordionPanel>
+          }
+      </AccordionItem>
+    )
+
+    const recipeInfo = (
+      <AccordionItem>
+        <AccordionButton>
+          <AccordionIcon ml={5} boxSize={8} />
+          <Heading
+            size='md'
+            ml={4}
+            my={5}
+          >
+            Used In
+          </Heading>
+        </AccordionButton>
+        {
+          recipes && recipes.length > 0 ? recipes.map(recipe => (
+            <RecipeCard recipe={recipe} />
+          )) : (
+            <AccordionPanel>
+              <Text ml={5} mb={3}>This food item is not used in any recipes</Text>
+            </AccordionPanel>
+          )
+        }
+      </AccordionItem>
+    )
+
+    return (<>
+      {headerInfo}
+      <Accordion width='100%' mt={5} allowToggle defaultIndex={0}>
+        {nutritionInfo}
+        {recipeInfo}
+      </Accordion>
+    </>);
 }
 
 export default Nutrition;
